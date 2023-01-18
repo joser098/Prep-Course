11f1b746f4f060e476579ce606b8b0ab073a6a3d
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-01-18 15:11:31 -0300

    Homework iniciada

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,13 +5,20 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) {
+    return x;
+  } else return y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18) {
+    return "Allowed"
+  } return "Not allowed";
 }
+
   
 function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
@@ -19,6 +26,11 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) {
+    return "Online";
+  }  if (status === 2) {
+    return "Away";
+  } return "Offline";
 }
 
 function saludo(idioma) {
@@ -28,6 +40,13 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") {
+    return "Guten Tag!";
+  } if (idioma === "mandarin") {
+    return "Ni Hao!";
+  } if (idioma === "ingles") {
+    return "Hello!";
+  } return "Hola!"
 }
 
 function colors(color) {
@@ -44,12 +63,18 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return true;
+  } return false;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero > 20) {
+    return true;
+  } return false;
 }
 
 function esEntero(numero) {
