3d6369c49ab388a9c30595ab67f6bc56e17094ec
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-01-20 14:35:12 -0300

    Homework avance

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -101,9 +101,11 @@ function esEntero(numero) {
   // Tu código:
   if (numero >= 0 || numero < 0) {
     return true;
-  }  return false;
+  } 
+  if(Number.isInteger(numero)) {
+    return false;
+  }return false;
 }
-
 
 function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
@@ -148,6 +150,12 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2) return false; 
+    for(var i = 2; i < numero; i++) {
+    if(numero % i === 0 ) {
+      return false;
+    }
+  }return true;
 }
 
 function esVerdadero(valor){
@@ -165,19 +173,31 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  for (var i = 0 ; i <= 60 ; i++) {
+    if(i % 6 === 0) {
+      return i;
+    }
+  }
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if(numero = numero >= 0 || numero < 0 ){
+    return true;
+  } if (numero.length === 3){
+    return true;
+} return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i = 0;
+  while (i <= i * 8) {
+    return i + 5;
+  }
 }
 
 
