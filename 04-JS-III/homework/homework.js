fd3d3958e4160b583d822b890fdf17afe6d6c973
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-01-25 12:58:11 -0300

    Homework Done

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -98,6 +98,11 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  let mayor = 0;
+  for(let i = 0; i < numeros.length; i++) {
+    if(numeros[i] > mayor) {
+      mayor = numeros[i] }
+  } return mayor;
 }
 
 
@@ -105,13 +110,26 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length === 0) {return 0;}
+  if (arguments.length === 1) {return 1;}
+
+  let multiplica = 1;
+  for(let i = 0; i < arguments.length; i++) {
+    multiplica = multiplica * arguments[i]
+  }
+  return multiplica;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  let contador = 0;
+  for(i = 0; i < arreglo.length; i++) {
+    if(arreglo[i] > 18) {
+      contador += 1;
+    }
+  } return contador;
 }
 
 
@@ -119,25 +137,30 @@ function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
-  //Escribe tu código aquí   
-  
-} 
+  //Escribe tu código aquí 
+  if(numeroDeDia === 7 || numeroDeDia === 1) return "Es fin de semana";  
+  if(numeroDeDia >= 2 || numeroDeDia < 7) return "Es dia Laboral";
+  } 
 
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  let str = String(n)[0];
+  if(Number(str) === 9) {return true;}
+  return false;
 }
 
 
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
-  
-} 
+  //Escribe tu código aquí 
+  for(let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] === arreglo[i+1]) {return true}
+  } return false;
+  } 
 
 
 function mesesDelAño(array) {
@@ -145,20 +168,29 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+let meses = [];
+for(let i = 0; i < array.length; i++) {
+  if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre") {
+    meses.push(array[i]);
+  }  
+} if(meses.length < 3) {
+  return "No se encontraron los meses pedidos";
+}else { return meses;
 }
-
+}
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
-  let mayores = [];
+ let nuevo = [];
+ for(i = 0; i < array.length; i++) {
+  if(array[i] > 100) {
+    nuevo.push(array[i]);
+  }
+ } return nuevo;
+} 
 
-  for(i = 0; i < 200; i++) {
-  mayores += array[i];
-  mayores.push(i)
-} return mayores;
-}
 
 
 function breakStatement(numero) {
@@ -169,7 +201,16 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  let nuevo = [];
+  for(i = 0; i < 10; i++) {
+    numero += 2;
+   if(numero === i) break;
+   else{nuevo.push(numero)};
   }
+  if(i < 10) {
+    return "Se interrumpió la ejecución";
+  } else {return nuevo;}
+}
   
 
 
@@ -181,7 +222,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+let nuevo = [];
+ for(let i = 0; i < 10; i++) {
+  if(i === 5) {
+    continue
+  } else {
+    numero += 2;
+    nuevo.push(numero)
+  }
+ }return nuevo;
 }
+
 
 
 // No modificar nada debajo de esta línea
